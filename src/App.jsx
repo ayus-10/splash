e368802d99c0205cb6f6485d14c3886c2cbedcd2
Aayush Upreti <136055230+ayus-10@users.noreply.github.com>
2023-11-24 20:54:09 +0545

    made the navbar functional

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import Nav from "./components/Nav.jsx";
 import Hero from "./components/Hero.jsx";
+import Services from "./components/Services.jsx";
+import Work from "./components/Work.jsx";
+import Contact from "./components/Contact.jsx";
 import "./App.css";
 
 const App = () => {
+  const [activeComponent, setActiveComponent] = useState("home");
+  const changeActiveComponent = (component) => {
+    setActiveComponent(component);
+  };
+
   return (
     <>
-      <Nav />
+      <Nav onButtonClick={changeActiveComponent} />
       <main className="wrapper">
-        <Hero />
+        {activeComponent === "home" && <Hero />}
+        {activeComponent === "services" && <Services />}
+        {activeComponent === "work" && <Work />}
+        {activeComponent === "contact" && <Contact />}
       </main>
     </>
   );
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,7 +3,7 @@ import "./Nav.css";
 import { FaBars } from "react-icons/fa6";
 import { AiOutlineClose } from "react-icons/ai";
 
-const Nav = () => {
+const Nav = (props) => {
   const [mobileNav, setMobileNav] = useState(false);
   const invertMobileNavState = () => {
     setMobileNav(!mobileNav);
@@ -12,20 +12,37 @@ const Nav = () => {
   return (
     <header className="nav">
       <div className="navbar">
-        <h1 className="nav-logo">Splash.</h1>
+        <h1 className="nav-logo" onClick={() => props.onButtonClick("home")}>
+          Splash.
+        </h1>
         <div className="nav-items">
-          <span className="nav-item">Services</span>
-          <span className="nav-item">Work</span>
-          <span className="nav-item">Contact</span>
+          <span
+            className="nav-item"
+            onClick={() => props.onButtonClick("services")}
+          >
+            Services
+          </span>
+          <span
+            className="nav-item"
+            onClick={() => props.onButtonClick("work")}
+          >
+            Work
+          </span>
+          <span
+            className="nav-item"
+            onClick={() => props.onButtonClick("contact")}
+          >
+            Contact
+          </span>
         </div>
         <a className="hamburger" onClick={invertMobileNavState}>
           {mobileNav ? <AiOutlineClose /> : <FaBars />}
         </a>
       </div>
       <div className={`mobile-nav ${mobileNav && "mobile-nav-visible"}`}>
-        <span>Services</span>
-        <span>Work</span>
-        <span>Contact</span>
+        <span onClick={() => props.onButtonClick("services")}>Services</span>
+        <span onClick={() => props.onButtonClick("work")}>Work</span>
+        <span onClick={() => props.onButtonClick("contact")}>Contact</span>
       </div>
     </header>
   );
