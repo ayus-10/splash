204e4fc36a20af2c44be4cdd2d9f8c3803c7b076
Aayush Upreti <136055230+ayus-10@users.noreply.github.com>
2023-11-25 15:31:54 +0545

    created service component

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,10 +1,63 @@
 import React from "react";
 import "./Services.css";
+import { FaImagePortrait } from "react-icons/fa6";
+import { RiCalendarEventLine } from "react-icons/ri";
+import {
+  MdEditSquare,
+  MdPets,
+  MdOutlineSettingsSuggest,
+  MdBusinessCenter,
+  MdCamera,
+} from "react-icons/md";
+import { TbDrone } from "react-icons/tb";
 
 const Services = () => {
+  const servicesList = [
+    {
+      name: "Portrait Photography",
+      icon: <FaImagePortrait />,
+    },
+    {
+      name: "Event Photography",
+      icon: <RiCalendarEventLine />,
+    },
+    {
+      name: "Commercial Photography",
+      icon: <MdBusinessCenter />,
+    },
+    {
+      name: "Pet Photography",
+      icon: <MdPets />,
+    },
+    {
+      name: "Social Media Content",
+      icon: <MdCamera />,
+    },
+    {
+      name: "Photo Editing",
+      icon: <MdEditSquare />,
+    },
+    {
+      name: "Drone Photography",
+      icon: <TbDrone />,
+    },
+    {
+      name: "Custom Packages",
+      icon: <MdOutlineSettingsSuggest />,
+    },
+  ];
+
   return (
     <div className="services-container" id="services">
-      Services
+      <h1 className="services-title">My Services</h1>
+      <ul className="services-list">
+        {servicesList.map((serviceItem) => (
+          <li className="list-item">
+            <span className="list-item-icon">{serviceItem.icon}</span>
+            <span className="list-item-text">{serviceItem.name}</span>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
