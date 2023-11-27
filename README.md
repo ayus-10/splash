29de8432eb9f5de0816c0f2c5b83bc400da12421
Aayush Upreti <136055230+ayus-10@users.noreply.github.com>
2023-11-27 17:50:04 +0545

    created contact component, added README.md

---
+++ README.md
@@ -0,0 +1,18 @@
+# About
+
+This is a Personal Portfolio webpage template created using React JS.
+
+![Demo Image](https://www.aayushupreti.com.np/assets/splash-18e61e74.png)
+
+# Installation
+
+1. First make sure that you have **npm** and **Node JS** installed.
+2. Clone this repo and open the folder in a terminal.
+3. Run these commands
+
+   ```
+    npm install
+    npm run build
+   ```
+
+4. The app will be built in the **dist** folder, run it using live server.
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,7 +4,29 @@ import "./Contact.css";
 const Contact = () => {
   return (
     <div className="contact-container" id="contact">
-      Contact
+      <div className="contact-form">
+        <h1 className="contact-title">Leave a message</h1>
+        <input
+          id="userName"
+          type="text"
+          placeholder="Type in your name..."
+          className="input-box"
+        />
+        <input
+          id="userEmail"
+          type="email"
+          placeholder="Type in your email..."
+          className="input-box"
+        />
+        <textarea
+          id="userMessage"
+          cols="30"
+          rows="3"
+          placeholder="And your message..."
+          className="input-box"
+        ></textarea>
+        <button>Send</button>
+      </div>
     </div>
   );
 };
